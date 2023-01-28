c085fe9d7828756a7aaa88490cd94efd5672fef1
docfrawley <docfrawley@gmail.com>
2023-01-28 11:08:48 -0500

    modal sytling

--- src/components/TechModal.vue
+++ src/components/TechModal.vue
@@ -1,34 +1,88 @@
 <template>
-    <div class="absolute top-10 mt-5 pt-5  inset-0 z-40 opacity-75 bg-pink-500 h-1/2 w-1/2">
-
-        <div class="bg-black w-4 float-right cursor-pointer" @click="closeModal">X</div>
-        <div class="text-2xl text-black opacity-100">Hello there</div>
-        <div>{{ item.dimension }}</div>
-        <div>{{ item.strategy }}</div>
-        
-    </div>
-
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="
+          absolute
+          w-full
+          bg-black bg-opacity-30
+          h-screen
+          top-0
+          left-0
+          flex
+          justify-center
+          px-8
+        "
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-sm"
+          >
+            <div class="text-2xl text-black opacity-100">Hello there</div>
+            <div class="text-2xl text-black opacity-100">
+              {{ mItem.dimension }}
+            </div>
+            <div class="text-2xl text-black opacity-100">
+              {{ mItem.strategy }}
+            </div>
+            <button
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+              @click="$emit('modalClose')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref } from "@vue/reactivity";
+import { Teleport, watchEffect } from "vue";
 export default {
-    props:['theTech'],
-    emits: ['modalClose'],
-    setup(props, context) {
-        const item = ref(props.theTech)
+  props: ["theTech", "modalActive"],
+  emits: ["modalClose"],
+  components: { Teleport },
+  setup(props, context) {
+    const mItem = ref({});
 
-        function closeModal() {
-            context.emit('modalClose')
-        }
-
-        return { item, closeModal}
-
-    }
-
-}
+    watchEffect(() => {
+      mItem.value = props.theTech;
+    });
+    return { mItem };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
 
-</style>>
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
--- src/views/MyToolkit.vue
+++ src/views/MyToolkit.vue
@@ -1,192 +1,230 @@
 <template>
-    <div class="toolkit-container">
-        <div class="drop-zone" >
-            <div v-for="item in items" :key="item.id" class="drag-row" draggable="true" 
-                @dragstart="startDrag($event, item)" @drop="onDrop($event, item)" @dragenter.prevent @dragover.prevent>
-                <div class="drag-el">{{ item.dimension }}</div>
-                <div v-for="strategy in item.techs" :key="item.dimension + strategy" draggable="true" 
-                    @dragstart="startDragRow($event, item, strategy)" @drop="onDropRow($event, item, strategy)" @dragenter.prevent @dragover.prevent>
-                    <div class="drag-strat">{{ strategy }}<svg @click="openModel(item.dimension, strategy)" xmlns="http://www.w3.org/2000/svg" height="24" width="24">
-                        <path
-                            d="M11.25 16.75h1.5V11h-1.5ZM12 9.3q.35 0 .575-.238.225-.237.225-.587 0-.325-.225-.563-.225-.237-.575-.237t-.575.237q-.225.238-.225.563 0 .35.225.587.225.238.575.238Zm0 12.2q-1.975 0-3.712-.75Q6.55 20 5.275 18.725T3.25 15.712Q2.5 13.975 2.5 12t.75-3.713Q4 6.55 5.275 5.275T8.288 3.25Q10.025 2.5 12 2.5t3.713.75q1.737.75 3.012 2.025t2.025 3.012q.75 1.738.75 3.713t-.75 3.712q-.75 1.738-2.025 3.013t-3.012 2.025q-1.738.75-3.713.75Zm0-1.5q3.35 0 5.675-2.325Q20 15.35 20 12q0-3.35-2.325-5.675Q15.35 4 12 4 8.65 4 6.325 6.325 4 8.65 4 12q0 3.35 2.325 5.675Q8.65 20 12 20Zm0-8Z" />
-                        </svg>
-                    </div>
-            </div>
+  <div class="toolkit-container">
+    <div class="drop-zone">
+      <div
+        v-for="item in items"
+        :key="item.id"
+        class="drag-row"
+        draggable="true"
+        @dragstart="startDrag($event, item)"
+        @drop="onDrop($event, item)"
+        @dragenter.prevent
+        @dragover.prevent
+      >
+        <div class="drag-el">{{ item.dimension }}</div>
+        <div
+          v-for="strategy in item.techs"
+          :key="item.dimension + strategy"
+          draggable="true"
+          @dragstart="startDragRow($event, item, strategy)"
+          @drop="onDropRow($event, item, strategy)"
+          @dragenter.prevent
+          @dragover.prevent
+        >
+          <div class="drag-strat">
+            {{ strategy
+            }}<svg
+              @click="openModel(item.dimension, strategy)"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              width="24"
+            >
+              <path
+                d="M11.25 16.75h1.5V11h-1.5ZM12 9.3q.35 0 .575-.238.225-.237.225-.587 0-.325-.225-.563-.225-.237-.575-.237t-.575.237q-.225.238-.225.563 0 .35.225.587.225.238.575.238Zm0 12.2q-1.975 0-3.712-.75Q6.55 20 5.275 18.725T3.25 15.712Q2.5 13.975 2.5 12t.75-3.713Q4 6.55 5.275 5.275T8.288 3.25Q10.025 2.5 12 2.5t3.713.75q1.737.75 3.012 2.025t2.025 3.012q.75 1.738.75 3.713t-.75 3.712q-.75 1.738-2.025 3.013t-3.012 2.025q-1.738.75-3.713.75Zm0-1.5q3.35 0 5.675-2.325Q20 15.35 20 12q0-3.35-2.325-5.675Q15.35 4 12 4 8.65 4 6.325 6.325 4 8.65 4 12q0 3.35 2.325 5.675Q8.65 20 12 20Zm0-8Z"
+              />
+            </svg>
+          </div>
         </div>
+      </div>
 
-        <div >
-            <button @click="handleReset" class="reset-button">RESET</button>
-            <button @click="handleTechs" class="reset-button">SAVE</button>
-        </div>
-
+      <div>
+        <button @click="handleReset" class="reset-button">RESET</button>
+        <button @click="handleTechs" class="reset-button">SAVE</button>
+      </div>
     </div>
-    
-<div v-if="showModal">
-    <TechModal @modalClose="handleClose" :theTech="strategyItems" />
-</div>
-</div>
- 
 
+    <TechModal
+      @modalClose="toggleModal"
+      :theTech="strategyItems"
+      :modalActive="modalActive"
+    />
+  </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
-import { userStore } from '@/store/userStore'
-import { coursesStore } from '@/store/coursesStore'
-import TechModal from '@/components/TechModal.vue'
+import { ref } from "@vue/reactivity";
+import { userStore } from "@/store/userStore";
+import { coursesStore } from "@/store/coursesStore";
+import TechModal from "@/components/TechModal.vue";
 
 export default {
-    components: { TechModal },
-    setup(){
-        const ustore = userStore()
-        const cstore = coursesStore()
-        const items = ref(cstore.currentCourse.techniques)
-        const original_items = ref(JSON.parse(JSON.stringify(items.value)))
-        const UserTechs = ref(ustore.getUserTechniques)
-        const showModal= ref(false)
-        const strategyItems = ref({})
+  components: { TechModal },
+  setup() {
+    const ustore = userStore();
+    const cstore = coursesStore();
+    const items = ref(cstore.currentCourse.techniques);
+    const original_items = ref(JSON.parse(JSON.stringify(items.value)));
+    const UserTechs = ref(ustore.getUserTechniques);
+    const modalActive = ref(false);
+    const strategyItems = ref({});
 
-        if (UserTechs.value.length>0){
-            items.value = UserTechs.value.currentAnswers
-        }
-
-        if (UserTechs.value.length>0){
-            items.value = UserTechs.value
-        }
-
-        const getList = (list) => {
-            return items.value.filter((item) => item.list==list)
-        }
-
-        const startDrag =(evt, item) =>{
-            const stringObject = JSON.stringify(item);
-            evt.dataTransfer.dropEffect = 'move'
-            evt.dataTransfer.effectAllowed = 'move'
-            evt.dataTransfer.setData('itemStuff', stringObject)
-        }
-
-        const onDrop = (evt, whereDrop) => {
-            const itemStuff = JSON.parse(evt.dataTransfer.getData('itemStuff'))
-            const whereStarted= items.value.findIndex(item => item.dimension===itemStuff.dimension)
-            const whereEnding = items.value.findIndex(item => item.dimension === whereDrop.dimension)
-            const tempObject = items.value[whereStarted]
-            items.value.splice(whereStarted, 1)
-            items.value.splice(whereEnding, 0, tempObject)
-        }
-
-        const startDragRow = (evt, item, tech) => {
-            const stringObject = JSON.stringify({
-                dimension: item.dimension,
-                tech: tech
-            });
-            evt.dataTransfer.dropEffect = 'move'
-            evt.dataTransfer.effectAllowed = 'move'
-            evt.dataTransfer.setData('itemStuffing', stringObject)
-        }
-
-        const onDropRow = (evt, whichitem, whereDrop) => {
-            const itemStuff = JSON.parse(evt.dataTransfer.getData('itemStuffing'))
-            const whereItem = items.value.findIndex(item => item.dimension === itemStuff.dimension)
-            if (whichitem.dimension === items.value[whereItem].dimension){
-                const whereStarted = whichitem.techs.findIndex(item => item == itemStuff.tech)
-                const whereEnding = whichitem.techs.findIndex(item => item === whereDrop)
-                const tempObject = whichitem.techs[whereStarted]
-                whichitem.techs.splice(whereStarted, 1)
-                whichitem.techs.splice(whereEnding, 0, tempObject)
-                items.value[whereItem].techs = whichitem.techs
-                ustore.updateTechs(items.value)
-            }
-        }
-
-        const openModel = (dimension, strategy)=>{
-            strategyItems.value = {
-                dimension:dimension,
-                strategy:strategy
-            }
-            showModal.value=true
-        }
-
-        const handleReset = () => {
-            
-            items.value = original_items.value
-            original_items.value = JSON.parse(JSON.stringify(items.value))
-            
-        }
-
-        const handleTechs = () => {
-
-            ustore.setTechniques()
-
-        }
-
-        const handleClose = () => {
-            showModal.value = false
-        }
-
-        return { getList, startDrag, onDrop, items, startDragRow, onDropRow, handleReset, handleTechs, showModal, handleClose, strategyItems, openModel }
+    if (UserTechs.value.length > 0) {
+      items.value = UserTechs.value.currentAnswers;
     }
 
-}
+    if (UserTechs.value.length > 0) {
+      items.value = UserTechs.value;
+    }
+
+    const getList = (list) => {
+      return items.value.filter((item) => item.list == list);
+    };
+
+    const startDrag = (evt, item) => {
+      const stringObject = JSON.stringify(item);
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("itemStuff", stringObject);
+    };
+
+    const onDrop = (evt, whereDrop) => {
+      const itemStuff = JSON.parse(evt.dataTransfer.getData("itemStuff"));
+      const whereStarted = items.value.findIndex(
+        (item) => item.dimension === itemStuff.dimension
+      );
+      const whereEnding = items.value.findIndex(
+        (item) => item.dimension === whereDrop.dimension
+      );
+      const tempObject = items.value[whereStarted];
+      items.value.splice(whereStarted, 1);
+      items.value.splice(whereEnding, 0, tempObject);
+    };
+
+    const startDragRow = (evt, item, tech) => {
+      const stringObject = JSON.stringify({
+        dimension: item.dimension,
+        tech: tech,
+      });
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("itemStuffing", stringObject);
+    };
+
+    const onDropRow = (evt, whichitem, whereDrop) => {
+      const itemStuff = JSON.parse(evt.dataTransfer.getData("itemStuffing"));
+      const whereItem = items.value.findIndex(
+        (item) => item.dimension === itemStuff.dimension
+      );
+      if (whichitem.dimension === items.value[whereItem].dimension) {
+        const whereStarted = whichitem.techs.findIndex(
+          (item) => item == itemStuff.tech
+        );
+        const whereEnding = whichitem.techs.findIndex(
+          (item) => item === whereDrop
+        );
+        const tempObject = whichitem.techs[whereStarted];
+        whichitem.techs.splice(whereStarted, 1);
+        whichitem.techs.splice(whereEnding, 0, tempObject);
+        items.value[whereItem].techs = whichitem.techs;
+        ustore.updateTechs(items.value);
+      }
+    };
+
+    const openModel = (dimension, strategy) => {
+      strategyItems.value = {
+        dimension: dimension,
+        strategy: strategy,
+      };
+      modalActive.value = true;
+    };
+
+    const handleReset = () => {
+      items.value = original_items.value;
+      original_items.value = JSON.parse(JSON.stringify(items.value));
+    };
+
+    const handleTechs = () => {
+      ustore.setTechniques();
+    };
+
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value;
+    };
+
+    return {
+      getList,
+      startDrag,
+      onDrop,
+      items,
+      startDragRow,
+      onDropRow,
+      handleReset,
+      handleTechs,
+      modalActive,
+      toggleModal,
+      strategyItems,
+      openModel,
+    };
+  },
+};
 </script>
 
 <style>
 .drop-zone {
-    display: flex;
-    flex-direction: column;
-    background-color: #ecf0f1;
-    margin-bottom: 10px;
-    padding: 10px;
-    width:70%;
-    min-height: 10px;
+  display: flex;
+  flex-direction: column;
+  background-color: #ecf0f1;
+  margin-bottom: 10px;
+  padding: 10px;
+  width: 70%;
+  min-height: 10px;
 }
 
-.drag-row{
-    display: flex;
-    flex-direction: row;
+.drag-row {
+  display: flex;
+  flex-direction: row;
 }
 
 .drag-el {
-    background-color: #3498db;
-    color:white;
-    margin: 5px;
-    padding: 5px;
-    width:200px;
-    text-align: center;
+  background-color: #3498db;
+  color: white;
+  margin: 5px;
+  padding: 5px;
+  width: 200px;
+  text-align: center;
 }
 
 .drag-strat {
-    background-color: red;
-    color: white;
-    margin: 5px;
-    padding: 5px;
-    width: 200px;
-    text-align: center;
-    display: relative;
+  background-color: red;
+  color: white;
+  margin: 5px;
+  padding: 5px;
+  width: 200px;
+  text-align: center;
+  display: relative;
 }
 
-.reset-button{
-    background-color: #3498db;
-    color:white;
-    width: 150px;
-    height: 50px;
-    margin-top: 50px;
-
+.reset-button {
+  background-color: #3498db;
+  color: white;
+  width: 150px;
+  height: 50px;
+  margin-top: 50px;
 }
 
-.toolkit-container{
-    padding-top: 200px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+.toolkit-container {
+  padding-top: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 
 svg {
-    display: inline;
-    float: right;
-    fill: white;
-    cursor: pointer;
+  display: inline;
+  float: right;
+  fill: white;
+  cursor: pointer;
 }
-
 
 /* .drag-el:nth-last-of-type(1){
     margin-bottom: 0;
